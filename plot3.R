678306c73b825da3ee74585335aeb3514c05cace
Raji SV <reginfo_r@yahoo.com>
2014-05-11 17:32:33 -0700

    plots

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@ plotdata <- read.csv("household_power_consumption_subset.txt", sep=";")
 
 png(filename="plot3.png", height= 480, width= 480)
 
-plot(plotdata$Sub_metering_1, type= "l", xlab="", ylab="Energy Sub metering")
+plot(plotdata$Sub_metering_1, type= "l", xlab="", ylab="Energy sub metering")
 lines(plotdata$Sub_metering_2, type="l", col="red")
 lines(plotdata$Sub_metering_3,type="l", col="blue")
 legend("topright", lty=1, col= c("black", "Red","blue"),legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
