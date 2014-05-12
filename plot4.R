cb5b7abe5d6476bbe0b411fed9d5b726d87394a9
Raji SV <reginfo_r@yahoo.com>
2014-05-11 17:24:00 -0700

    plot4

---
+++ plot4.R
@@ -0,0 +1,19 @@
+plotdata <- read.csv("household_power_consumption_subset.txt", sep=";")
+png(filename="plot4.png", height= 480, width= 480)
+
+
+par(mfrow=c(2,2))
+
+
+        plot(plotdata$Global_active_power, type= "l", xlab="", ylab="Global Active Power")
+
+        plot(plotdata$Voltage, type= "l", xlab="datetime", ylab="Voltage")
+
+        plot(plotdata$Sub_metering_1, type= "l", xlab="", ylab="Energy Submetering")
+        lines(plotdata$Sub_metering_2, type="l", col="red")
+        lines(plotdata$Sub_metering_3,type="l", col="blue")
+        legend("topright", lty=1, col= c("black", "Red","blue"),legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+        plot(plotdata$Global_reactive_power, type="l",xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
